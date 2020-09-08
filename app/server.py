fef128069f7e161e6c56ef670c2adb4641e37e64
saraj79 <70947654+saraj79@users.noreply.github.com>
2020-09-08 21:25:37 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -14,7 +14,9 @@ import PIL
 
 #export_file_url = 'https://drive.google.com/file/d/16IsnIGNNlMTKCv_fpXZ7OYbfCJAcVEig/view?usp=sharing'
 #export_file_url="https://drive.google.com/uc?export=download&id=16IsnIGNNlMTKCv_fpXZ7OYbfCJAcVEig"
-export_file_url="https://drive.google.com/uc?export=download&confirm=no_antivirus&id=16IsnIGNNlMTKCv_fpXZ7OYbfCJAcVEig"
+#export_file_url="https://drive.google.com/uc?export=download&confirm=no_antivirus&id=16IsnIGNNlMTKCv_fpXZ7OYbfCJAcVEig"
+export_file_url=https://drive.google.com/u/0/uc?export=download&confirm=QZ9_&id=16IsnIGNNlMTKCv_fpXZ7OYbfCJAcVEig
+
 export_file_name = 'full_model_export1.pkl'
 
 data_transforms_with_normalization = {
